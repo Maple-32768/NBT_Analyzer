6447372a29c5062522808c6909b05be0c3d398a9
Maple-32768 <ondouro58@gmail.com>
2021-10-20 19:23:55 +0900

    move analyze method

--- src/TAGComponent.java
+++ src/TAGComponent.java
@@ -1,4 +1,63 @@
+import java.util.Arrays;
+
 public abstract class TAGComponent {
     abstract public TAGHeader getHeader();
     abstract public String toString();
+
+    public static TAGComponent Analyze(byte[] data){
+        TAGComponent result = null;
+        TAGHeader header = TAGHeader.getHeader(data);
+        byte[] data1 = Arrays.copyOfRange(data, header.length, data.length);
+        switch (header.type){
+            case 0:
+                //todo:tag_end
+                break;
+
+            case 1:
+                result = new TAGByte(header, data1);
+                break;
+
+            case 2:
+                result = new TAGShort(header, data1);
+                break;
+
+            case 3:
+                result = new TAGInt(header, data1);
+                break;
+
+            case 4:
+                result = new TAGLong(header, data1);
+                break;
+
+            case 5:
+                result = new TAGFloat(header, data1);
+                break;
+
+            case 6:
+                result = new TAGDouble(header, data1);
+                break;
+
+            case 7:
+                result = new TAGByteArray(header, data1);
+                break;
+
+            case 8:
+                result = new TAGString(header, data1);
+                break;
+
+            case 9:
+                //todo:tag_list
+                break;
+
+            case 10:
+                //todo:tag_compound
+                break;
+
+            case 11:
+                result = new TAGIntArray(header, data1);
+                break;
+
+        }
+        return result;
+    }
 }
--- src/TestNBT.java
+++ src/TestNBT.java
@@ -13,65 +13,8 @@ public class TestNBT {
 				(byte)0x80,(byte)0x00,(byte)0x00,(byte)0x00,
 				(byte)0xff,(byte)0xff,(byte)0xff,(byte)0xff
 		};
-		TAGComponent c = Analyze(data);
+		TAGComponent c = TAGComponent.Analyze(data);
 		System.out.println(c.getHeader().tagName + " : " + c);
-	}
-
-	public static TAGComponent Analyze(byte[] data){
-		TAGComponent result = null;
-		TAGHeader header = TAGHeader.getHeader(data);
-		byte[] data1 = Arrays.copyOfRange(data, header.length, data.length);
-		switch (header.type){
-			case 0:
-				//todo:tag_end
-				break;
-
-			case 1:
-				result = new TAGByte(header, data1);
-				break;
-
-			case 2:
-				result = new TAGShort(header, data1);
-                break;
-
-            case 3:
-                result = new TAGInt(header, data1);
-                break;
-
-            case 4:
-                result = new TAGLong(header, data1);
-                break;
-
-			case 5:
-				result = new TAGFloat(header, data1);
-				break;
-
-			case 6:
-				result = new TAGDouble(header, data1);
-				break;
-
-			case 7:
-				result = new TAGByteArray(header, data1);
-				break;
-
-			case 8:
-				result = new TAGString(header, data1);
-				break;
-
-			case 9:
-				//todo:tag_list
-				break;
-
-			case 10:
-				//todo:tag_compound
-				break;
-
-			case 11:
-				result = new TAGIntArray(header, data1);
-				break;
-
-		}
-		return result;
 	}
 
 }
