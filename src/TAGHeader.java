17baaf4954a9b85bd8a51a60a7eaf11bfc638053
Maple-32768 <ondouro58@gmail.com>
2021-10-24 03:40:44 +0900

    getInstance()の追加

--- src/TAGHeader.java
+++ src/TAGHeader.java
@@ -1,4 +1,8 @@
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class TAGHeader {
@@ -10,13 +14,25 @@ public class TAGHeader {
 	public String tag_name;
 	public int size;
 
-	public TAGHeader(byte[] data) {
+	public TAGHeader(byte @NotNull [] data) {
 		this.type = data[0];
 		if(this.type == 0x00) return;
 		this.name_size = ByteBuffer.wrap(Arrays.copyOfRange(data, type_length, type_length + name_size_length)).getShort();
 		if (this.name_size == 0) this.tag_name = null;
 		else this.tag_name = new String(Arrays.copyOfRange(data, type_length + name_size_length, type_length + name_size_length + this.name_size));
-		this.size = 3 + this.name_size;
+		this.size = calculateSize();
+	}
+
+	public TAGHeader(byte type, String name){
+		this.type = type;
+		if(this.type == 0x00) return;
+		this.tag_name = name;
+		this.name_size = (short) name.getBytes(StandardCharsets.UTF_8).length;
+		this.size = this.calculateSize();
+	}
+
+	public int calculateSize(){
+		return this.size = 3 + this.name_size;
 	}
 
 	public String getTagName(boolean json){
@@ -25,15 +41,17 @@ public class TAGHeader {
 		return result;
 	}
 
-	public String getTagName(){
-		return this.getTagName(false);
+	public static @NotNull TAGHeader getInstance(byte type, @NotNull String name){
+		return new TAGHeader(type, name);
 	}
 
-	public static TAGHeader getHeader(byte[] raw_data){
+	@Contract("_ -> new")
+	public static @NotNull TAGHeader getHeader(byte[] raw_data){
 		return new TAGHeader(raw_data);
 	}
 
-	public static TAGHeader getNullHeader(byte type){
+	public static @NotNull
+	TAGHeader getNullHeader(byte type){
 		TAGHeader result = new TAGHeader(new byte[] {(byte)0x00});
 		result.type = type;
 		return result;
